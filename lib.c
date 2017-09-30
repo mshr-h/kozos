2e4688ac5939aebbd1144d932080cd28004d3e39
mshr-h <mshr-h@users.noreply.github.com>
2017-09-30 22:46:30 +0900

    chapter 2: add library functions

--- lib.c
+++ lib.c
@@ -2,6 +2,76 @@
 #include "serial.h"
 #include "lib.h"
 
+void *memset(void *b, int c, long len) {
+	char *p;
+	for (p = b; len > 0; len--) {
+		*p = c;
+		p++;
+	}
+	return b;
+}
+
+void *memcpy(void *dst, const void *src, long len) {
+	char *d = dst;
+	const char *s = src;
+	for (; len > 0; len--) {
+		*d = *s;
+		d++;
+		s++;
+	}
+	return d;
+}
+
+int memcmp(const void *b1, const void *b2, long len) {
+	const char *p1 = b1;
+	const char *p2 = b2;
+	for (; len > 0; len--) {
+		if (*p1 != *p2)
+			return (*p1 > *p2) ? 1 : -1;
+		p1++;
+		p2++;
+	}
+	return 0;
+}
+
+int strlen(const char *s) {
+	int len;
+	for (len = 0; *s; len++)
+		;
+	return len;
+}
+
+char *strcpy(char *dst, const char *src) {
+	char *d = dst;
+	for (; ; d++, src++) {
+		*d = *src;
+		if (*src == 0)
+			break;
+	}
+	return d;
+}
+
+int strcmp(const char *s1, const char *s2) {
+	while (*s1 || *s2) {
+		if (*s1 != *s2)
+			return (*s1 > *s2) ? 1 : -1;
+		s1++;
+		s2++;
+	}
+	return 0;
+}
+
+int strncmp(const char *s1, const char *s2, int len) {
+	while ((*s1 || *s2) && (len > 0)) {
+		if (*s1 != *s2)
+			return (*s1 > *s2) ? 1 : -1;
+		s1++;
+		s2++;
+		len--;
+	}
+	return 0;
+}
+
 int putc(unsigned char c) {
 	if (c == '\n') {
 		serial_send_byte(SERIAL_DEFAULT_DEVICE, '\r');
@@ -10,10 +80,34 @@ int putc(unsigned char c) {
 }
 
 int puts(unsigned char *str) {
-	while(*str) {
+	while (*str) {
 		putc(*str);
 		str++;
 	}
+	return 0;
+}
+
+int putxval(unsigned long value, int column) {
+	char buf[9];
+	char *p;
+
+	p = buf + sizeof(buf) - 1;
+	*p = '\0';
+	p--;
+
+	if (!value && !column)
+		column++;
+
+	while (value || column) {
+		*p = "0123456789abcdef"[value & 0xf];
+		p--;
+		value >>= 4;
+		if (column)
+			column--;
+	}
+
+	puts(p+1);
+
 	return 0;
 }
 
