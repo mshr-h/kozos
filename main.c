e6937c499419c14b281170c80861166705b2e0b5
mshr-h <mshr-h@users.noreply.github.com>
2017-10-06 22:34:55 +0900

    add chapter 3

--- main.c
+++ main.c
@@ -2,16 +2,40 @@
 #include "serial.h"
 #include "lib.h"
 
-int main(void) {
+static init(void) {
+	extern int erodata, data_start, edata, bss_start, ebss;
+
+	// initialize data and bss sections
+	memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+	memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
 	serial_init(SERIAL_DEFAULT_DEVICE);
+}
+
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+static void printval(void) {
+	puts("global_data = "); putxval(global_data, 0); puts("\n");
+	puts("global_bss  = "); putxval(global_bss,  0); puts("\n");
+	puts("static_data = "); putxval(static_data, 0); puts("\n");
+	puts("static_bss  = "); putxval(static_bss,  0); puts("\n");
+}
+
+int main(void) {
+	init();
 
 	puts("Hello, world!\n");
 
-	putxval(0x10, 0);
-	puts("\n");
-
-	putxval(0xffff, 0);
-	puts("\n");
+	printval();
+	puts("overrite variables.\n");
+	global_data = 0x20;
+	global_bss  = 0x30;
+	static_data = 0x40;
+	static_bss  = 0x50;
+	printval();
 
 	while(1)
 		;
